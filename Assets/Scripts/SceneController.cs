71684503049532e7c204b1e66a7cb00e66d28281
NafeeJ <nafeejan2@gmail.com>
2020-12-14 23:03:36 -0500

    Implemented Confession, Consume, Controller Smash, Driving, Fitness Shark, Gold Mining, Holobirds, Interrupted by Bird, Karaoke, Minecraft Rap, Polkatsu, Reading, Rolling, Spicy Noodles, and Ukulele Practice.

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -20,10 +20,25 @@ public class SceneController : MonoBehaviour
 
         objs = Resources.LoadAll<GameObject>("Prefabs/HoloPrefabs"); //INSERT PATH OF OBJECT PREFABS;
         objSizes = new float[objs.Length];
-        objSizes[0] = 0.22f; //birdSize
-        objSizes[1] = 0.2f; //bonkSize
-        objSizes[2] = 0.22f; //booksSize
-        objSizes[3] = 1f; //thisIsTrueSize
+        objSizes[0] = 0.22f; //bird
+        objSizes[1] = 0.2f; //bonk
+        objSizes[2] = 0.2f; //books
+        objSizes[3] = 0.2f; //confession
+        objSizes[4] = 0.2f; //consume noodles
+        objSizes[5] = 0.18f; //controller smash
+        objSizes[6] = 0.17f; //driving
+        objSizes[7] = 0.2f; //fitness shark
+        objSizes[8] = 0.25f; //gold mining
+        objSizes[9] = 0.19f; //holobirds
+        objSizes[10] = 0.2f; //interrupted by bird
+        objSizes[11] = 0.22f; //karaoke
+        objSizes[12] = 0.17f; //minecraft rap
+        objSizes[13] = 0.18f; //polkatsu
+        objSizes[14] = 0.22f; //reading
+        objSizes[15] = 0.2f; //rolling
+        objSizes[16] = 0.18f; //spicy noodles
+        objSizes[17] = 1f; //this is true
+        objSizes[18] = 0.18f; //ukulele practice
     }
 
     void Update()
